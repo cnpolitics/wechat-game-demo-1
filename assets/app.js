0232ba1bbdbe21b1a59fbad79355ec51f8dd7392
Zhengyu Qian <laerhsif@gmail.com>
2015-04-17 23:47:29 +0800

    Handle the user interaction in one specific question. + `touchOption` function to handle the click/touch event of the options. + `myCast` array to record the user's options.

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,26 @@ $(document).ready(function() {
 		sectionSelector: '.js-page'
 	});
 });
+
+var myCast = [0, 0, 0, 0, 0];
+
+function touchOption(optID, optNo) {
+	$(optID + ' .opt').on('touchstart', function(){
+		$(this).addClass('pressing');
+	});
+	
+	$(optID + ' .opt').on('click touchend', function(){
+		$(optID + ' .opt.active').removeClass('active');
+		$(this).removeClass('pressing').addClass('active');
+		myCast[optNo] = $(this).index();
+//		console.log(myCast);
+	});
+}
+
+$(window).load(function() {
+	touchOption('#q1', 0);
+	touchOption('#q2', 1);
+	touchOption('#q3', 2);
+	touchOption('#q4', 3);
+	touchOption('#q5', 4);
+});
