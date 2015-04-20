dcfc842b4f750b75e7a8d20abc4ad5fc0a1613f5
Zhengyu Qian <laerhsif@gmail.com>
2015-04-20 17:25:42 +0800

    Add the method of result calculation (by Yuyu Wang).

---
+++ javascript/answer.js
@@ -0,0 +1,27 @@
+function answer(arr){
+	var k = 0,d = 0,w = 0,t = 0;
+	var param = [["k","w","t","d"],["t","w","k","d"],["w","k","t","d"],["d","t","w","d"],["d","k","t","w"]]
+	for (i=0; i<5; i++) {
+		type = param[i][arr[i]];
+		add(type);
+	};
+	a = [k,d,w,t];
+	b = ["k","d","w","t"];
+	max = a.indexOf(Math.max.apply(Math, a));
+	return max;
+}
+
+function add(type){
+	if (type == "k"){
+		k = k + 1;
+	}
+	else if (type == "d"){
+		d = d + 1;
+	}
+	else if (type == "w"){
+		w = w + 1;
+	}
+	else if (type == "t"){
+		t = t + 1;
+	}
+}
