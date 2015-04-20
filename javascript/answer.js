7ec00e06627737220757d093077562dbf4006542
Zhengyu Qian <laerhsif@gmail.com>
2015-04-21 01:16:28 +0800

    Debug `answer.js`. - Fix syntax errors. - Fix global variables definitions. - Remove unused vars.

--- javascript/answer.js
+++ javascript/answer.js
@@ -1,3 +1,4 @@
+/*// by Yuyu Wang
 function answer(arr){
 	var k = 0,d = 0,w = 0,t = 0;
 	var param = [["k","w","t","d"],["t","w","k","d"],["w","k","t","d"],["d","t","w","d"],["d","k","t","w"]]
@@ -8,6 +9,7 @@ function answer(arr){
 	a = [k,d,w,t];
 	b = ["k","d","w","t"];
 	max = a.indexOf(Math.max.apply(Math, a));
+	console.log('answer');
 	return max;
 }
 
@@ -24,4 +26,35 @@ function add(type){
 	else if (type == "t"){
 		t = t + 1;
 	}
+}
+*/
+
+var k = 0,d = 0,w = 0,t = 0;
+
+function add(type){
+	if (type === "k"){
+		k = k + 1;
+	}
+	else if (type === "d"){
+		d = d + 1;
+	}
+	else if (type === "w"){
+		w = w + 1;
+	}
+	else if (type === "t"){
+		t = t + 1;
+	}
+}
+
+function answer(arr){
+	var param = [["k","w","t","d"],["t","w","k","d"],["w","k","t","d"],["d","t","w","d"],["d","k","t","w"]];
+	var a = [k,d,w,t];
+	var type;
+	
+	for (var i=0; i<5; i++) {
+		type = param[i][arr[i]];
+		add(type);
+	}
+	console.log('answer');
+	return a.indexOf(Math.max.apply(Math, a));
 }
