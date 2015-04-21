8bfe718da9c88226480767a660d6308b6d9126aa
Zhengyu Qian <laerhsif@gmail.com>
2015-04-21 18:19:39 +0800

    Make the layout for the `.omission-guide` in the result page.

--- javascript/main.js
+++ javascript/main.js
@@ -70,13 +70,16 @@ $(window).load(function() {
 		$.fn.fullpage.moveSectionDown();
 	});
 	
-	// Interactions and actions of the `.btn-share`
-	$('.btn-share').on('touchstart', function(){
-		$(this).addClass('pressing');
-	});
-	$('.btn-share').on('touchend', function(){
+	// Interactions of the `.btn-hex`
+	$('.btn-hex').on('touchstart', function(){
+			$(this).addClass('pressing');
+		});
+	$('.btn-hex').on('touchend', function(){
 		$(this).removeClass('pressing');
-
+	});
+	
+	// Actions of the `.btn-share`
+	$('.btn-share').on('touchend', function(){
 		$('#share-guide').fadeIn(250);
 		$('#share-guide').on('touchend', function(){
 			$(this).fadeOut(250);
